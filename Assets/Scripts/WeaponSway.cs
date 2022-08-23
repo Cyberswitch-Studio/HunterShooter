b8dcfc784ac2f4878831c8788d8b3dc696728538
BartyDash <writetobartek@gmail.com>
2022-08-23 23:51:21 +0200

    weapon sway

--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Sway Settings")]
+    [SerializeField] private float smooth;
+    [SerializeField] private float swayMultiplayer;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplayer;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplayer;
+
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
     }
 }
